b0f2dc7b9008ab06259c6090321c7336130af80b
Gabriel Rodríguez <gabrc52@gmail.com>
2023-03-11 00:03:39 -0500

    Make helper functions "private"

--- krb-wrapper.py
+++ krb-wrapper.py
@@ -17,7 +17,7 @@ from tempfile import NamedTemporaryFile
 # there is already an ASN.1 implementation, but in the interest of
 # limiting MIT Kerberos's exposure to malformed ccaches, encode it
 # ourselves. To that end, here's the laziest DER encoder ever.
-def der_encode_length(l: int) -> bytes:
+def _der_encode_length(l: int) -> bytes:
     if l <= 127:
         return bytes([l])
     out: bytes = b""
@@ -27,10 +27,10 @@ def der_encode_length(l: int) -> bytes:
     out = bytes([len(out) | 0x80]) + out
     return out
 
-def der_encode_tlv(tag: int, value) -> bytes:
-    return bytes([tag]) + der_encode_length(len(value)) + value
+def _der_encode_tlv(tag: int, value) -> bytes:
+    return bytes([tag]) + _der_encode_length(len(value)) + value
 
-def der_encode_integer_value(val: int) -> bytes:
+def _der_encode_integer_value(val: int) -> bytes:
     # base 256, MSB first, two's complement, minimum number of octets
     # necessary. This has a number of annoying edge cases:
     # * 0 and -1 are 0x00 and 0xFF, not the empty string.
@@ -50,24 +50,24 @@ def der_encode_integer_value(val: int) -> bytes:
         val >>= 8
     return out
 
-def der_encode_integer(val: int) -> bytes:
-    return der_encode_tlv(0x02, der_encode_integer_value(val))
-def der_encode_int32(val: int) -> bytes:
+def _der_encode_integer(val: int) -> bytes:
+    return _der_encode_tlv(0x02, _der_encode_integer_value(val))
+def _der_encode_int32(val: int) -> bytes:
     if val < -2147483648 or val > 2147483647:
         raise ValueError("Bad value")
-    return der_encode_integer(val)
-def der_encode_uint32(val: int) -> bytes:
+    return _der_encode_integer(val)
+def _der_encode_uint32(val: int) -> bytes:
     if val < 0 or val > 4294967295:
         raise ValueError("Bad value")
-    return der_encode_integer(val)
+    return _der_encode_integer(val)
 
-def der_encode_string(val: str) -> bytes:
-    return der_encode_tlv(0x1b, val.encode("utf-8"))
+def _der_encode_string(val: str) -> bytes:
+    return _der_encode_tlv(0x1b, val.encode("utf-8"))
 
-def der_encode_octet_bytes(val: bytes) -> bytes:
-    return der_encode_tlv(0x04, val)
+def _der_encode_octet_bytes(val: bytes) -> bytes:
+    return _der_encode_tlv(0x04, val)
 
-def der_encode_sequence(tlvs: list[bytes | None], tagged: bool=True) -> bytes:
+def _der_encode_sequence(tlvs: list[bytes | None], tagged: bool=True) -> bytes:
     body: list[bytes] = []
     for i, tlv in enumerate(tlvs):
         # Missing optional elements represented as None.
@@ -75,68 +75,68 @@ def der_encode_sequence(tlvs: list[bytes | None], tagged: bool=True) -> bytes:
             continue
         if tagged:
             # Assume kerberos-style explicit tagging of components.
-            tlv = der_encode_tlv(0xa0 | i, tlv)
+            tlv = _der_encode_tlv(0xa0 | i, tlv)
         body.append(tlv)
-    return der_encode_tlv(0x30, b"".join(body))
+    return _der_encode_tlv(0x30, b"".join(body))
 
-def der_encode_ticket(tkt: dict) -> bytes:
-    return der_encode_tlv(
+def _der_encode_ticket(tkt: dict) -> bytes:
+    return _der_encode_tlv(
         0x61, # Ticket
-        der_encode_sequence(
-            [der_encode_integer(5), # tktVno
-             der_encode_string(tkt["realm"]),
-             der_encode_sequence( # PrincipalName
-                    [der_encode_int32(tkt["sname"]["nameType"]),
-                     der_encode_sequence([der_encode_string(c)
+        _der_encode_sequence(
+            [_der_encode_integer(5), # tktVno
+             _der_encode_string(tkt["realm"]),
+             _der_encode_sequence( # PrincipalName
+                    [_der_encode_int32(tkt["sname"]["nameType"]),
+                     _der_encode_sequence([_der_encode_string(c)
                                           for c in tkt["sname"]["nameString"]],
                                          tagged=False)]),
-             der_encode_sequence( # EncryptedData
-                    [der_encode_int32(tkt["encPart"]["etype"]),
-                     (der_encode_uint32(tkt["encPart"]["kvno"])
+             _der_encode_sequence( # EncryptedData
+                    [_der_encode_int32(tkt["encPart"]["etype"]),
+                     (_der_encode_uint32(tkt["encPart"]["kvno"])
                       if "kvno" in tkt["encPart"]
                       else None),
-                     der_encode_octet_bytes(
+                     _der_encode_octet_bytes(
                                 base64.b64decode(tkt["encPart"]["cipher"]))])]))
 
 # Kerberos ccache writing code. Using format documentation from here:
 # http://www.gnu.org/software/shishi/manual/html_node/The-Credential-Cache-Binary-File-Format.html
 
-def ccache_counted_octet_bytes(data: bytes) -> bytes:
+def _ccache_counted_octet_bytes(data: bytes) -> bytes:
     return struct.pack("!I", len(data)) + data
 
-def ccache_principal(name: dict, realm: str) -> bytes:
+def _ccache_principal(name: dict, realm: str) -> bytes:
     header: bytes = struct.pack("!II", name["nameType"], len(name["nameString"]))
-    return (header + ccache_counted_octet_bytes(realm.encode('utf-8')) +
-            b"".join(ccache_counted_octet_bytes(c.encode("utf-8"))
+    return (header + _ccache_counted_octet_bytes(realm.encode('utf-8')) +
+            b"".join(_ccache_counted_octet_bytes(c.encode("utf-8"))
                     for c in name["nameString"]))
 
-def ccache_key(key: dict) -> bytes:
+def _ccache_key(key: dict) -> bytes:
     return (struct.pack("!H", key["keytype"]) +
-            ccache_counted_octet_bytes(base64.b64decode(key["keyvalue"])))
+            _ccache_counted_octet_bytes(base64.b64decode(key["keyvalue"])))
 
-def flags_to_uint32(flags: list) -> int:
+def _flags_to_uint32(flags: list) -> int:
     ret = 0
     for i, v in enumerate(flags):
         if v:
             ret |= 1 << (31 - i)
     return ret
 
-def ccache_credential(cred: dict) -> bytes:
-    out: bytes = ccache_principal(cred["cname"], cred["crealm"])
-    out += ccache_principal(cred["sname"], cred["srealm"])
-    out += ccache_key(cred["key"])
+def _ccache_credential(cred: dict) -> bytes:
+    out: bytes = _ccache_principal(cred["cname"], cred["crealm"])
+    out += _ccache_principal(cred["sname"], cred["srealm"])
+    out += _ccache_key(cred["key"])
     out += struct.pack("!IIII",
                        cred["authtime"] // 1000,
                        cred.get("starttime", cred["authtime"]) // 1000,
                        cred["endtime"] // 1000,
                        cred.get("renewTill", 0) // 1000)
     out += struct.pack("!B", 0)
-    out += struct.pack("!I", flags_to_uint32(cred["flags"]))
+    out += struct.pack("!I", _flags_to_uint32(cred["flags"]))
     # TODO: Care about addrs or authdata? Former is "caddr" key.
     out += struct.pack("!II", 0, 0)
-    out += ccache_counted_octet_bytes(der_encode_ticket(cred["ticket"]))
+    out += _ccache_counted_octet_bytes(_der_encode_ticket(cred["ticket"]))
     # No second_ticket.
-    out += ccache_counted_octet_bytes(b"")
+    out += _ccache_counted_octet_bytes(b"")
     return out
 
 def make_ccache(cred: dict) -> bytes:
@@ -149,8 +149,8 @@ def make_ccache(cred: dict) -> bytes:
                       8, # taglen (two uint32_ts)
                       0, 0, # time_offset / usec_offset
                       )
-    out += ccache_principal(cred["cname"], cred["crealm"])
-    out += ccache_credential(cred)
+    out += _ccache_principal(cred["cname"], cred["crealm"])
+    out += _ccache_credential(cred)
     return out
 
 # SSH-specific code skipped
