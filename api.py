0eb460a1ddfe02decf46b99a90a54e2e85bcb5ae
Gabriel Rodríguez <gabrc52@gmail.com>
2023-03-11 02:32:47 -0500

    remove redundant decorator

--- api.py
+++ api.py
@@ -1,11 +1,9 @@
 import json
-from decorators import json_api
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-@json_api
 def index_get():
     return {
         'hello': 'world',
--- decorators.py
+++ decorators.py
@@ -1,7 +1,1 @@
 from flask import jsonify
-
-def json_api(func):
-    def decorated(*args, **kwargs):
-        return jsonify(func(*args, **kwargs))
-    return decorated
-
