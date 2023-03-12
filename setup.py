6bac4c2785bffde93e95325ccebdb448a93f7619
Gabriel Rodríguez <gabrc52@gmail.com>
2023-03-12 01:20:44 -0500

    Missed one

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
     author="Gabriel Rodríguez",
     author_email="rgabriel@mit.edu",
     license="MIT",
-    py_modules=["api", "decorators", "make_ccache"],
+    py_modules=["api", "decorators", "make_ccache", "util"],
     # TODO: might the name(s) conflict?
     # In theory we should only need to export one (api right now)
     # But we need to `import decorators`
     # Maybe something like https://fortierq.github.io/python-import/ is what we need?
+    # Maybe subfolders?
 )
