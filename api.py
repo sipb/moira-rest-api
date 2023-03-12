9a83a35bd4cc77a4c467451731fc2a905ef6fc42
Gabriel Rodríguez <gabrc52@gmail.com>
2023-03-11 19:54:51 -0500

    Prepare to write API

--- api.py
+++ api.py
@@ -1,9 +1,15 @@
-import json
 import subprocess
-from flask import Flask, request, jsonify
-from decorators import webathena, plaintext
+from flask import Flask, request
+from decorators import webathena, plaintext, moira_query
+import moira
 
 app = Flask(__name__)
+
+# I wanted to separate this into multiple files, but it seems
+# non-trivial: 
+# https://www.reddit.com/r/flask/comments/m3kp1i/splitting_flask_app_into_multiple_files/
+# https://flask.palletsprojects.com/en/2.2.x/patterns/appfactories/
+# So I think one file will suffice
 
 @app.get('/test')
 @webathena
@@ -12,12 +18,22 @@ def test():
     result = subprocess.run('klist', stdout=subprocess.PIPE)
     return result.stdout.decode()
 
-@app.route('/')
-def index():
+@app.errorhandler(404)
+def not_found(error):
     return {
-        'hello': 'world',
-        'name': request.args.get('name'),
+        'name': 'METHOD_NOT_FOUND',
+        'description': f'{error}',
     }
 
+@app.get('/users/<string:user>')
+@webathena
+@moira_query
+def get_user(user):
+    res = moira.query('get_user_by_login', user)
+    assert len(res) == 1
+    return res[0]
+
+# TODO: don't do this in production
+app.debug = True
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
