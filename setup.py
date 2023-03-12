c582d00cc3352959a84e5b8a31a339daf2267274
Gabriel Rodríguez <gabrc52@gmail.com>
2023-03-12 01:18:48 -0500

    Add other modules to available imports as pip module

--- setup.py
+++ setup.py
@@ -7,5 +7,9 @@ setup(
     author="Gabriel Rodríguez",
     author_email="rgabriel@mit.edu",
     license="MIT",
-    py_modules=["api"], # TODO: might it conflict? doesn't matter if venv though
+    py_modules=["api", "decorators", "make_ccache"],
+    # TODO: might the name(s) conflict?
+    # In theory we should only need to export one (api right now)
+    # But we need to `import decorators`
+    # Maybe something like https://fortierq.github.io/python-import/ is what we need?
 )
